ebd684b81995fee7db861370bc76594140e7427a
jitesh <jitesh.kumar05official@gmail.com>
2025-03-05 17:16:11 +0530

    learning started

---
+++ img_process/image.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageFilter
+
+img = Image.open('./images/pikachu.webp')
+
+# filtered_img = img.filter(ImageFilter.BLUR)
+# print(img.format)
+# print(img.size)
+# print(img.mode)
+# img.show()
+# print(dir(img))
+# print(filtered_img.format)
+# filtered_img.show()
+# filtered_img.save('./images/pikachu_blur.png', 'png')
+# filtereed_img = img.filter(ImageFilter.SHARPEN)
+# give title to the image
+# filtereed_img.show()
+# filtered_img = img.convert('L')
+# filtered_img.show()
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+# filtered_img.show()
+# filtered_img = filtered_img.rotate(90)
+# filtered_img.show()
